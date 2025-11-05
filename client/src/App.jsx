858ff23a064b94a4a23a9053a956cbd3b09e3ef8
eh <hlzemile@gmail.com>
2025-11-05 18:14:23 +0100

    feat: Added some necessary for building the game (cells, blocks, game)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import IndexPage from './pages/IndexPage';
-import SinglePage from './pages/SinglePage';
+import IndexPage from './pages/IndexPage/IndexPage';
+import SinglePlayerPage from './pages/SinglePlayerPage/SinglePlayerPage';
 
 function App() {
   return (
@@ -9,7 +9,7 @@ function App() {
         {/* Redirection automatique de "/" vers "/index" */}
         <Route path="/" element={<Navigate to="/index" replace />} />
         <Route path="/index" element={<IndexPage />} />
-        <Route path="/singleplayer" element={<SinglePage />} />
+        <Route path="/singleplayer" element={<SinglePlayerPage />} />
       </Routes>
     </BrowserRouter>
   );
