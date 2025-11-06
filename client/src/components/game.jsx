3ddf2e70941c088f9e62b701bfbc0c0546ab8281
madegryc <mathis.koro@gmail.com>
2025-11-06 16:39:50 +0100

    add block backend button

---
+++ client/src/components/game.jsx
@@ -0,0 +1,20 @@
+const COLONNES = 10;
+const LIGNES = 20;
+const TOTAL_LIGNES = 24;
+
+const grille = Array.from({ length: TOTAL_LIGNES }, () =>
+    Array.from({ length: COLONNES }, () => 0)
+    );
+
+function game() {
+  return (
+    
+    <>
+      <div>Game Component</div>
+        
+        
+    </>
+  );
+}
+
+export default game;
--- client/src/pages/SinglePlayerPage/SinglePlayerPage.jsx
+++ client/src/pages/SinglePlayerPage/SinglePlayerPage.jsx
@@ -1,61 +1,48 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { io } from 'socket.io-client';
 import './SinglePlayerPage.css';
 
 const SIZEX = 10;
 const SIZEY = 20;
-const blocks = {
-	I: [
-		[1, 1, 1, 1]
-	],
-	J: [
-		[1, 0, 0],
-		[1, 1, 1]
-	],
-	L: [
-		[0, 0, 1],
-		[1, 1, 1]
-	],
-	O: [
-		[1, 1],
-		[1, 1]
-	],
-	S: [
-		[0, 1, 1],
-		[1, 1, 0]
-	],
-	T: [
-		[0, 1, 0],
-		[1, 1, 1]
-	],
-	Z: [
-		[1, 1, 0],
-		[0, 1, 1]
-	],
-};
+
+// Connexion au serveur WebSocket
+const socket = io('http://localhost:5000');
 
 function SinglePlayerPage() {
     const [grid, setGrid] = useState(
         Array.from({ length: SIZEY }, () => Array(SIZEX).fill(0))
     );
 
-	const addRandomBlock = () => {
-		const blockTypes = Object.keys(blocks);
-		const randomType = blockTypes[Math.floor(Math.random() * blockTypes.length)];
-		const block = blocks[randomType];
-		
-		setGrid(prevGrid => {	
-			const newGrid = Array.from({ length: SIZEY }, () => Array(SIZEX).fill(0)).map(row => row.slice());
-			
-			for (let y = 0; y < block.length; y++) {
-				for (let x = 0; x < block[y].length; x++) {
-					if (block[y][x]) {
-						newGrid[y][x + Math.floor((SIZEX - block[y].length) / 2)] = 1;
-					}
-				}
-			}
-			return newGrid;
-		});
-	};
+    useEffect(() => {
+        socket.on('tetromino', ({ type, shape }) => {
+            console.log('🎮 Tétrimino reçu:', type, shape);
+
+            setGrid(() => {
+                const newGrid = Array.from({ length: SIZEY }, () => Array(SIZEX).fill(0));
+
+                for (let y = 0; y < shape.length; y++) {
+                    for (let x = 0; x < shape[y].length; x++) {
+                        if (shape[y][x]) {
+                            const gridX = x + Math.floor((SIZEX - shape[y].length) / 2);
+                            if (gridX >= 0 && gridX < SIZEX && y < SIZEY) {
+                                newGrid[y][gridX] = 1;
+                            }
+                        }
+                    }
+                }
+                return newGrid;
+            });
+        });
+
+        return () => {
+            socket.off('tetromino');
+        };
+    }, []);
+
+    const requestTetromino = () => {
+        console.log('📡 Demande d\'un nouveau tétrimino...');
+        socket.emit('requestTetromino');
+    };
 
     return (
         <div className="singleplayer-component">
@@ -82,7 +69,7 @@ function SinglePlayerPage() {
 						<h2>Score</h2>
 						<p>0</p>
 					</div>
-					<button className="singleplayer-start-button" onClick={addRandomBlock}>Add block</button>
+					<button className="singleplayer-start-button" onClick={requestTetromino}>Add block</button>
 				</div>
             </div>
         </div>
